e929ec753b638de999b4453c60e4641e44440c96
Alexandru Moisescu <moisescu.alexandru88@gmail.com>
2018-08-18 16:49:44 +0300

    Implemented the router for the nav-bar

--- src/Nav.js
+++ src/Nav.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Nav extends Component {
 	render() {
 		return(
 			<div className="nav">
-				<div className="button">Home</div>
-				<div className="button">Projects</div>
-				<div className="button">Contact</div>
+				<div className="button"><Link to='/'>Home</Link></div>
+				<div className="button" onMouseOver={() => document.getElementById('projects').classList.add('show')}>
+					Projects
+					<div id='projects' className="projects" onMouseOut={() => document.getElementById('projects').classList.remove('show')}>
+						<Link to='/neighborhood-map'><div className='project'>Neighborhood Map</div></Link>
+						<Link to='/arcade-game'><div className='project'>Arcade Game</div></Link>
+						<Link to='/memory-game'><div className='project'>Memory Game</div></Link>
+						<Link to='/pixel-art-maker'><div className='project'>Pixel Art Maker</div></Link>
+						<Link to='/myreads'><div className='project'>My Reads</div></Link>
+					</div>
+				</div>
+				<Link to='/contacts' className="button"><div>Contact</div></Link>
 			</div>
 		)
 	}
